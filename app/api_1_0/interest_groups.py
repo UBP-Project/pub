9026309261d16bd25977ea6624068fce5e880e1e
acondense <ariel.conde1997@gmail.com>
2017-07-21 19:05:11 +0800

    CRUD API for Interest Groups

--- app/api_1_0/interest_groups.py
+++ app/api_1_0/interest_groups.py
@@ -1,12 +1,20 @@
 from flask import jsonify, request, url_for
 from .. import db
 import json
-from ..models import Interest_Group, Activity
+from ..models import Interest_Group
 from . import api
 
 @api.route('/interest_groups/')
 def get_interest_groups():
-    return "Query List of Interest_Group"
+    interest_groups = Interest_Group.query.all()
+    return jsonify({
+        'interest_groups': [interest_group.to_json() for interest_group in interest_groups]
+    })
+
+@api.route('/interest_groups/<int:id>')
+def get_interest_group(id):
+    interest_group = Interest_Group.query.get_or_404(id)
+    return jsonify(interest_group.to_json())
 
 @api.route('/interest_groups/', methods=['POST'])
 def new_interest_group():
@@ -16,3 +24,15 @@ def new_interest_group():
     db.session.commit()
     return jsonify(interest_group.to_json()), 201, \
         {'Location': url_for('api.new_interest_group', id=interest_group.id, _external=True)}
+
+@api.route('/interest_groups/<int:id>', methods=['PUT'])
+def edit_interest_group(id):
+    interest_group = Interest_Group.query.filter_by(id=id).update(request.form.to_dict())
+    db.session.commit()
+    return "Update Success" # change this to better message format
+
+@api.route('/interest_groups/<int:id>', methods=['DELETE'])
+def delete_interest_group(id):
+    interest_group = Interest_Group.query.filter_by(id=id).delete()
+    db.session.commit()
+    return "Delete Success"
