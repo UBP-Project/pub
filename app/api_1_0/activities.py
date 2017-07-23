dad818b74250e58268e7b97402c1ef96606e6b26
acondense <ariel.conde1997@gmail.com>
2017-07-23 12:10:02 +0800

    Modified API URI

--- app/api_1_0/activities.py
+++ app/api_1_0/activities.py
@@ -4,19 +4,19 @@ from .. import db
 from . import api
 import json
 
-@api.route('/activity/')
+@api.route('/activities/')
 def get_activities():
     activities = Activity.query.all()
     return jsonify({
         'activities': [activity.to_json() for activity in activities]
     })
 
-@api.route('/activity/<int:id>/')
+@api.route('/activities/<int:id>/')
 def get_activity(id):
-    activity = activity.query.get_or_404(id)
+    activity = Activity.query.get_or_404(id)
     return jsonify(activity.to_json())
 
-@api.route('/activity/', methods=['POST'])
+@api.route('/activities/', methods=['POST'])
 def new_activity():
     data = request.form.to_dict()
     activity = Activity.from_json(data)
@@ -25,13 +25,13 @@ def new_activity():
     return jsonify(activity.to_json()), 201, \
         {'Location': url_for('api.new_activity', id=activity.id, _external=True)}
 
-@api.route('/activity/<int:id>', methods=['PUT'])
+@api.route('/activities/<int:id>', methods=['PUT'])
 def edit_activity(id):
     activity = Activity.query.filter_by(id=id).update(request.form.to_dict())
     db.session.commit()
     return "Update Success" # change this to better message format
 
-@api.route('/activity/<int:id>', methods=['DELETE'])
+@api.route('/activities/<int:id>', methods=['DELETE'])
 def delete_activity(id):
     activity = Activity.query.filter_by(id=id).delete()
     db.session.commit()
