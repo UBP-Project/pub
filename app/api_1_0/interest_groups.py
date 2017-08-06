e07e5ad1852879fdda6cd3769d6aa91abb0900e0
Red Periabras <redperiabras@gmail.com>
2017-08-06 22:34:00 +0800

    Revised POST request for creating group

--- app/api_1_0/interest_groups.py
+++ app/api_1_0/interest_groups.py
@@ -29,10 +29,14 @@ def get_interest_group(id):
 def new_interest_group():
     data = request.form.to_dict()
     interest_group = Interest_Group.from_json(data)
-    db.session.add(interest_group)
-    db.session.commit()
-    return jsonify(interest_group.to_json()), 201, \
-        {'Location': url_for('api.new_interest_group', id=interest_group.id, _external=True)}
+    try:
+        db.session.add(interest_group)
+        db.session.commit()
+        return jsonify(interest_group.to_json()), 201, \
+            {'Location': url_for('api.new_interest_group', id=interest_group.id, _external=True)}
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify({'message': 'Name already taken'}), 409
 
 @api.route('/interest_groups/<int:id>', methods=['PUT'])
 def edit_interest_group(id):
