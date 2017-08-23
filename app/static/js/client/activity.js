58e51b02c2631e36084bea36a1fabcbf47c14067
Prince Julius Hari <princejulius230@gmail.com>
2017-08-24 00:57:31 +0800

    Going to activity users list

--- app/static/js/client/activity.js
+++ app/static/js/client/activity.js
@@ -5,7 +5,7 @@
 function view_activity(activity_id) {
     console.log("ACT_ID = " + activity_id);
     interested_users(activity_id);
-    //going_users(activity_id);
+    going_users(activity_id);
 }
 
 function interested_users(activity_id) {
@@ -14,21 +14,19 @@ function interested_users(activity_id) {
         type: 'GET'
     })
     .done(function(data) {
-        console.log("success");
+        //console.log("success");
         if(data.length){
-            console.log("May interested hehe");
             var users_list = "";
             for (var i in data) {
                 var name = data[i].firstname + " " + data[i].lastname;
                 var user_id = data[i].id;
                 users_list += "<a href='/profile/" + user_id + "'>" + name + "</a><br/>";
-                console.log("NAME: " + name);
-                console.log("USER_ID: " + user_id);
+                /*console.log("NAME: " + name);
+                console.log("USER_ID: " + user_id);*/
             }
             document.getElementById('interested-users-'+activity_id).innerHTML = users_list;
         } else {
-            document.getElementById('interested-users-'+activity_id).innerHTML = 'No one is interested'; 
-            console.log("No interesado");  
+            document.getElementById('interested-users-'+activity_id).innerHTML = 'No one is interested to this activity';   
         }
     })
     .fail(function() {
@@ -42,9 +40,20 @@ function going_users(activity_id) {
         type: 'GET'
     })
     .done(function(data) {
-        console.log("success");
-        console.log(data);
-        console.log(data.length);
+         //console.log("success");
+        if(data.length){
+            var users_list = "";
+            for (var i in data) {
+                var name = data[i].firstname + " " + data[i].lastname;
+                var user_id = data[i].id;
+                users_list += "<a href='/profile/" + user_id + "'>" + name + "</a><br/>";
+                /*console.log("NAME: " + name);
+                console.log("USER_ID: " + user_id);*/
+            }
+            document.getElementById('going-users-'+activity_id).innerHTML = users_list;
+        } else {
+            document.getElementById('going-users-'+activity_id).innerHTML = 'No one is going to this activity';   
+        }
     })
     .fail(function() {
         console.log("error");
@@ -53,15 +62,15 @@ function going_users(activity_id) {
 
 
 
-/*
-$('.activity-card').bind('click', function(event) {
+
+$('.activity-btn').bind('click', function(event) {
 
     var activity_id = event.currentTarget.id;  // activity id  
     var btn = event.currentTarget;
     console.log("ACT_ID = " + activity_id);
     console.log(btn);
 
-});*/
+});
 
 function going(activity_id) {
     $.ajax({
