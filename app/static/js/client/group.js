937bd6c8ad6537f66ac4056f657ed62ee36ed729
Prince Julius Hari <princejulius230@gmail.com>
2017-09-04 08:19:00 +0800

    UI issue: View leader per group fixed

--- app/static/js/client/group.js
+++ app/static/js/client/group.js
@@ -19,24 +19,14 @@ $('.grp-btn').bind('click', function(event) {
             $(btn).addClass('btn-warning');
             $(btn).text('CANCEL JOIN REQUEST');
             event.currentTarget.value = 'cancel';
-            console.log("Join Request Sent");
+            //console.log("Join Request Sent");
+            console.log("success");
         })
         .fail(function() {
              console.log("error");
         });
 
     } else {
-
-        $.ajax({
-            url: '/api/v1.0/interest_groups/'+ group_id + '/members',
-            type: 'GET'
-        })
-        .done(function(data) {
-            console.log("GET MEMBERS DATA -> " + data);
-        })
-        .fail(function() {
-             console.log("error");
-        });
 
         $.ajax({
             url: '/api/v1.0/interest_groups/'+ group_id + '/leave',
@@ -49,7 +39,8 @@ $('.grp-btn').bind('click', function(event) {
             $(btn).text('JOIN GROUP');
             event.currentTarget.value = 'join';
             viewMembers(group_id);
-            console.log("You left the group");
+            //console.log("You left the group");
+            console.log('success');
         })
         .fail(function() {
              console.log("error");
@@ -65,10 +56,38 @@ $('.grp-btn').bind('click', function(event) {
 */
 
 $('.grp-icon').bind('click', function(event) {
-    var group_id = event.currentTarget.id;  // group id  
+    var group_id = event.currentTarget.id;  // group id
+    viewLeaders(group_id);
     viewMembers(group_id);
     viewActivities(group_id);
 });
+
+function viewLeaders(group_id) {
+
+    $.ajax({
+        url: '/api/v1.0/interest_groups/'+ group_id + '/leaders',
+        type: 'GET'
+    })
+    .done(function(data) {
+
+        if(data.length){
+            var users_list = "<b>Leader(s)</b><br/>";
+            for (var i in data) {
+                var name = data[i].firstname + " " + data[i].lastname;
+                var user_id = data[i].id;
+                users_list += "<a href='/profile/" + user_id + "'>" + name + "</a><br/>";
+            }
+            document.getElementById('group-leaders-'+group_id).innerHTML = users_list;
+        } else {
+            document.getElementById('group-leaders-'+group_id).innerHTML = '<i>Group has no leaders</i>';   
+        }
+
+    })
+    .fail(function() {
+        console.log("error");
+    });
+
+}
 
 function viewMembers(group_id) {
 
@@ -79,17 +98,15 @@ function viewMembers(group_id) {
     .done(function(data) {
 
         if(data.length){
-            var users_list = "";
+            var users_list = "<b>Member(s)</b><br/>";
             for (var i in data) {
                 var name = data[i].firstname + " " + data[i].lastname;
                 var user_id = data[i].id;
                 users_list += "<a href='/profile/" + user_id + "'>" + name + "</a><br/>";
-                /*console.log("NAME: " + name);
-                console.log("USER_ID: " + user_id);*/
             }
             document.getElementById('group-members-'+group_id).innerHTML = users_list;
         } else {
-            document.getElementById('group-members-'+group_id).innerHTML = 'Group has no members';   
+            document.getElementById('group-members-'+group_id).innerHTML = '<i>Group has no members</i>';   
         }
 
     })
@@ -116,14 +133,14 @@ function viewActivities(group_id) {
             }
 
             if(activity_list==""){
-                document.getElementById('group-activities-'+group_id).innerHTML = "Group has No Activity"; 
+                document.getElementById('group-activities-'+group_id).innerHTML = "<i>Group has No Activity</i>"; 
             } else {
                 document.getElementById('group-activities-'+group_id).innerHTML = activity_list;
-                console.log(activity_list);
+                //console.log(activity_list);
             }
             
         } else {
-            document.getElementById('group-activities-'+group_id).innerHTML = "Group has No Activity";
+            document.getElementById('group-activities-'+group_id).innerHTML = "<i>Group has No Activity</i>";
             console.log('Group has No Activity');
         }
 
