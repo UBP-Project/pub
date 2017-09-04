5c2b6feb1d50d44b8a6b1943045ccea523f51ef8
acondense <ariel.conde1997@gmail.com>
2017-09-05 03:53:07 +0800

    Modified activities client view

--- app/api_1_0/activities.py
+++ app/api_1_0/activities.py
@@ -74,7 +74,7 @@ def get_activities():
         page = 1
 
     activities = Activity.query\
-        .paginate(page = page, per_page = 12, error_out=False)
+        .paginate(page = page, per_page = 8, error_out=False)
 
     return jsonify({
       'activities': [ activity.to_json() for activity in activities.items ],
