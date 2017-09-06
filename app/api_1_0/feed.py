7b8b947f4cc11592f1d1cf07857c2e69f600527b
acondense <ariel.conde1997@gmail.com>
2017-09-06 11:01:32 +0800

    Home page

--- app/api_1_0/feed.py
+++ app/api_1_0/feed.py
@@ -15,25 +15,55 @@ def serialize(model):
 
 @api.route('/feed')
 def feed():
+    return membership_feed()
+
+def membership_feed():
     feed = Follow.query.join(Membership, Membership.user_id == Follow.following_id)\
-        .add_columns(Membership.group_id, Membership.date_joined)\
+        .add_columns(Membership.group_id, Membership.timestamp)\
         .join(User, Follow.following_id == User.id)\
-        .add_columns(User.firstname, User.id.label("user_id"))\
+        .add_columns(User.firstname, User.lastname, User.id.label("user_id"),
+            User.image.label("user_image"))\
         .join(Interest_Group, Interest_Group.id == Membership.group_id)\
-        .add_columns(Interest_Group.name, Interest_Group.id.label("group_id"))\
+        .add_columns(Interest_Group.name, Interest_Group.id.label("group_id"),
+            Interest_Group.name.label("group_name"),
+            Interest_Group.cover_photo.label("group_cover"),
+            Interest_Group.group_icon.label("group_icon"),
+            Interest_Group.about.label("group_about"))\
         .filter(Follow.follower_id == current_user.get_id())\
-        .order_by(Membership.date_joined.desc())\
+        .order_by(Membership.timestamp.desc())\
         .distinct()
     return jsonify({
         'feed': [membership_to_json(f) for f in feed]
     })
 
+# def user_activity_feed():
+#     feed = Follow.query.join(User_Activity, User_Activity.user_id == Follow.following_id)\
+#         .add_columns(User_Activity.group_id, User_Activity.timestamp)\
+#         .join(User, Follow.following_id == User.id)\
+#         .add_columns(User.firstname, User.lastname, User.id.label("user_id"),
+#             User.image.label("user_image"))\
+#         .join(Activity, Activity.id == User_Activity.activity_id)\
+#         .add_columns(Interest_Group.name, Interest_Group.id.label("group_id"),
+#             Interest_Group.name.label("group_name"))\
+#         .filter(Follow.follower_id == current_user.get_id())\
+#         .order_by(Membership.timestamp.desc())\
+#         .distinct()
+#     return jsonify({
+#         'feed': [membership_to_json(f) for f in feed]
+#     })
+
 def membership_to_json(feed_item):
     membership = {
-        'user_firstname'  : feed_item.firstname,
-        'user_date_joined': feed_item.date_joined,
-        'group_name'      : feed_item.name,
-        'group_id'        : feed_item.group_id,
-        'user_id'         : feed_item.user_id
+        'type'          : 'group_membership',
+        'user_id'       : feed_item.user_id,
+        'user_firstname': feed_item.firstname,
+        'user_lastname' : feed_item.lastname,
+        'user_image'    : feed_item.user_image,
+        'group_name'    : feed_item.group_name,
+        'group_id'      : feed_item.group_id,
+        'group_icon'    : feed_item.group_icon,
+        'group_cover'   : feed_item.group_cover,
+        'group_about'   : feed_item.group_about,
+        'timestamp'     : feed_item.timestamp
     }
     return membership
