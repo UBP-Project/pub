34694682f83cf62ad8eb6979261090335c7ba11a
Prince Julius Hari <princejulius230@gmail.com>
2017-09-19 11:54:53 +0800

    Update my interested activities

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -467,15 +467,18 @@ def get_followings(id):
             following.isFollowing = False
 
     return jsonify({
-        'followings': [{
-            'id'         : follow_item.id,
-            'firstname'  : follow_item.firstname,
-            'lastname'   : follow_item.lastname,
-            'department' : follow_item.department,
-            'position'   : follow_item.position,
-            'image'      : follow_item.image,
-            'isFollowing': follow_item.isFollowing
-        }for following in followings]
+        'followings': [follow_item_to_json(following) for following in followings]
+    })
+
+def follow_item_to_json(follow_item):
+    return ({
+        'id'         : follow_item.id,
+        'firstname'  : follow_item.firstname,
+        'lastname'   : follow_item.lastname,
+        'department' : follow_item.department,
+        'position'   : follow_item.position,
+        'image'      : follow_item.image,
+        'isFollowing': follow_item.isFollowing
     })
 
 @api.route('/leaderboard')
