b847feb4bd7010c652ebcc1cb0f5f57a1bba31ed
Prince Julius Hari <princejulius230@gmail.com>
2017-10-12 23:39:52 +0800

    Error 500 Page

--- app/client/errors.py
+++ app/client/errors.py
@@ -12,3 +12,7 @@ def page_not_found(e):
 @client.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('error/500.html'),	500
+
+@client.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('error/500.html'),	500
