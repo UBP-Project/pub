80a8d1d58bcb7b685671c9515c2a59543cbfd2a1
Prince Julius Hari <princejulius230@gmail.com>
2017-10-12 23:58:03 +0800

    Error 400 Page

--- app/client/errors.py
+++ app/client/errors.py
@@ -1,6 +1,10 @@
 from flask import render_template, jsonify, request
 from . import client
 
+@client.app_errorhandler(400)
+def bad_request_error(e):
+    return render_template('error/400.html'),	400
+
 @client.app_errorhandler(401)
 def unathorized_error(e):
     return render_template('error/401.html'),	401
@@ -20,3 +24,7 @@ def page_not_found(e):
 @client.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('error/500.html'),	500
+
+@client.app_errorhandler(503)
+def service_unavailable_error(e):
+    return render_template('error/503.html'),	503
