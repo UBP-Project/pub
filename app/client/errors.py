6c5d303f55391dd3fe0dabfaedf90b8c605d9f13
Prince Julius Hari <princejulius230@gmail.com>
2017-10-12 23:44:25 +0800

    Error 403 Page

--- app/client/errors.py
+++ app/client/errors.py
@@ -9,9 +9,9 @@ def page_not_found(e):
 		return response
 	return render_template('error/404.html'),	404
 
-@client.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template('error/500.html'),	500
+@client.app_errorhandler(403)
+def forbidden_error(e):
+    return render_template('error/403.html'),	403
 
 @client.app_errorhandler(500)
 def internal_server_error(e):
