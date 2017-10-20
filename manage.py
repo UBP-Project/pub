064ee20da692ef643c21ba98bd0cc66b2403466c
redperiabras <redperiabras@gmail.com>
2017-10-20 10:56:46 +0800

    Added Create admin shell command

--- manage.py
+++ manage.py
@@ -15,12 +15,38 @@ manager = Manager(app)
 server = Server(host="0.0.0.0", port=5000)
 manager.add_command("runserver", server)
 
+def create_admin():
+	print("Create an administrator")
+	firstname = input("Firstname: ")
+	lastname = input("Lastname: ")
+	email = input("Email: ")
+	password = input("Password: ")
+	department = input("Department: ")
+	position = input("Position: ")
+	admin_role = Role.query.filter(Role.name == "Administrator").first()
+
+	admin_json = {
+		'firstname': firstname,
+		'lastname': lastname,
+		'email': email,
+		'password': password,
+		'department': department,
+		'position': position
+	}
+
+	admin = User.from_json(admin_json)
+	admin.role_id = admin_role.id
+	admin.password = password
+	db.session.add(admin)
+	db.session.commit()
+
 def make_shell_context():
     return dict(app=app, db=db, Interest_Group=Interest_Group,
         User=User, Role=Role,
         seed=seed,
         simulate=simulate,
-        test=test)
+        test=test,
+        create_admin=create_admin)
     
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
